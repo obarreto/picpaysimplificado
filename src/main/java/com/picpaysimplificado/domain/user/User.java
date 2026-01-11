9135f9c2ee32c4e0941d19c13626c90b4b3f2ad3
obarreto <ozbarreto@outlook.com>
2026-01-11 14:11:09 -0300

    create User entity

---
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,40 @@
+package com.picpaysimplificado.domain.user;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+    
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
